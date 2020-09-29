949d64588fb4fd0d4d2a50747a076a2f0713d956
DP <ec2-user@ip-172-31-30-62.ec2.internal>
2020-09-29 22:19:06 +0000

    added dotenv function

--- food.py
+++ food.py
@@ -3,8 +3,15 @@ import flask
 import random
 import os
 import sys
+import requests
+from os.path import join, dirname
+from dotenv import load_dotenv
+import json
 
 app = flask.Flask(__name__)
+
+dotenv_path = join(dirname(__file__), 'food.env')
+load_dotenv(dotenv_path)
 
 consumer_key= os.environ['CONSUMER_KEY']
 consumer_secret = os.environ['CONSUMER_SECRET']
@@ -14,6 +21,8 @@ access_token_secret = os.environ['ACCESS_TOKEN_SECRET']
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 auth_api = tweepy.API(auth)
+
+spoonacular_key = os.environ['SPOONACULAR_KEY']
 
 @app.route('/') 
 def index():
